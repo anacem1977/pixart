9c2913d8ae1d3d7be4139e309f4c853fe7d231f4
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-04 15:49:53 -0600

    cloned HW-started JS

--- pixart/pixart.js
+++ pixart/pixart.js
@@ -1,1 +1,11 @@
+console.log("Up and running");
 
+const form = document.querySelector("form");
+form.addEventListener("submit", getColor)
+
+function getColor(event) {
+    event.preventDefault();
+    const colorField = document.querySelector("#color-field");
+    console.log(colorField.value);
+    document.body.style.backgroundColor = colorField.value; 
+}
